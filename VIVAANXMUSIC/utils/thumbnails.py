9b87a6016d369daf1f8a3e3a682bf1bcf6c44e40
ɪᴍ_ᴘʀᴏᴛʀɪᴄᴋs <115210962+Protricks@users.noreply.github.com>
2026-06-18 14:22:28 +0530

    Update thumbnails.py

--- VIVAANXMUSIC/utils/thumbnails.py
+++ VIVAANXMUSIC/utils/thumbnails.py
@@ -4,7 +4,7 @@ import re
 import aiofiles
 import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
-from youtubesearchpython.future import VideosSearch
+from py_yt import VideosSearch
 from config import YOUTUBE_IMG_URL
 
 # Constants
